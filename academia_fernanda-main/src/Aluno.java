ca0d393b958cd23c7dc6dce806d3224b85653cb3
PedroRaphaelG <pedroraphaelgon@hotmail.com>
2024-11-28 23:38:52 -0300

    Adição de objetos no package models

---
+++ academia_fernanda-main/src/Aluno.java
@@ -0,0 +1,40 @@
+import enums.FrequenciaSemanal;
+import enums.NivelTreino;
+import enums.Plano;
+import models.Pessoa;
+
+public class Aluno extends Pessoa {
+    private Plano plano;
+    private boolean pagamentoEmDia;
+    private NivelTreino nivelTreino;
+    private FrequenciaSemanal frequencia;
+
+    public Aluno(String nome, int idade, String cpf, String genero, String endereco, Plano plano, NivelTreino nivelTreino, FrequenciaSemanal frequencia) {
+        super(nome, idade, cpf, genero, endereco);
+        this.plano = plano;
+        this.nivelTreino = nivelTreino;
+        this.frequencia = frequencia;
+        this.pagamentoEmDia = false;
+    }
+
+    public NivelTreino getNivelTreino() {
+        return nivelTreino;
+    }
+
+    public FrequenciaSemanal getFrequencia() {
+        return frequencia;
+    }
+
+    public void registrarPagamento() {
+        this.pagamentoEmDia = true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+               ", enums.Plano: " + plano +
+               ", Nível: " + nivelTreino + 
+               ", Frequência: " + frequencia.getDias() + " dias/semana, " +
+               "Pagamento em dia: " + (pagamentoEmDia ? "Sim" : "Não");
+    }
+}
---
+++ academia_fernanda-main/src/models/Professor.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Professor extends Pessoa {
+    private String especialidade;
+
+    public Professor(String nome, int idade, String cpf, String genero, String endereco, String especialidade) {
+        super(nome, idade, cpf, genero, endereco);
+        this.especialidade = especialidade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+               ", Especialidade: " + especialidade;
+    }
+}
